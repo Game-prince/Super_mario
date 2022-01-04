30a62c75dee5e5fb286719e81cc3014aa41cc697
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-01-04 17:37:54 +0530

    Sliced spreadhsheet in usable format

--- Assets/Scripts/backgroundScript.cs
+++ Assets/Scripts/backgroundScript.cs
@@ -1,13 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class backgroundScript : MonoBehaviour
 {
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("I am going to make this game");
     }
 
 }
