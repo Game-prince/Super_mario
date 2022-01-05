4e396705056cba7de7234df8ead832cd7cb1aaaf
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-01-05 21:22:50 +0530

    Added player animation

--- Assets/Scripts/playerscript.cs
+++ Assets/Scripts/playerscript.cs
@@ -1,18 +1,32 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class playerscript : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float playerForce = 3000f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        playerForce *= Time.deltaTime;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
+        if (Input.GetKeyDown("right"))
+        {
+            rb.AddForce(new Vector2(playerForce, 0));
+        }
+        if (Input.GetKeyUp("right")) rb.AddForce(new Vector2(-playerForce, 0));
+
+        if (Input.GetKeyDown("left"))
+        {
+            rb.AddForce(new Vector2(-playerForce, 0));
+        }
+        if (Input.GetKeyUp("left")) rb.AddForce(new Vector2(playerForce, 0));
     }
 }
