7795ece07c9af3aaae18e96d92aefb03ead81985
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-01-08 21:18:23 +0530

    almost got the idea of timemap

--- Assets/Scripts/playerscript.cs
+++ Assets/Scripts/playerscript.cs
@@ -4,57 +4,15 @@ using UnityEngine;
 
 public class playerscript : MonoBehaviour
 {
-    private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
-    private float playerSpeed = 5.0f;
-    private float jumpHeight = 1.0f;
-    private float gravityValue = 0;
     private Animator anim;
 
     private void Start()
     {
-        controller = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         float forward = Input.GetAxis("Horizontal"), upward = Input.GetAxis("Vertical");
-        controller.Move(new Vector3(playerSpeed * forward, 0, 0) * Time.deltaTime);
-
-        if (forward != 0) 
-        {
-            anim.SetFloat("speed", 1);
-            if (forward < 0) transform.eulerAngles = new Vector3(0, 180, 0);
-            else transform.eulerAngles = new Vector3(0, 0, 0);
-        }
-        else anim.SetFloat("speed", 0);
-
-        if (upward != 0) anim.SetBool("isJumping", true);
-        else anim.SetBool("isJumping", false);
-
-        // groundedPlayer = controller.isGrounded;
-        // if (groundedPlayer && playerVelocity.y < 0)
-        // {
-        //     playerVelocity.y = 0f;
-        // }
-
-        // Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        // controller.Move(move * Time.deltaTime * playerSpeed);
-
-        // if (move != Vector3.zero)
-        // {
-        //     transform.forward = move;
-        // }
-
-        // // Changes the height position of the player..
-        // if (Input.GetButtonDown("Jump") && groundedPlayer)
-        // {
-        //     playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        // }
-
-        // playerVelocity.y += gravityValue * Time.deltaTime;
-        // controller.Move(playerVelocity * Time.deltaTime);
     }
 }
